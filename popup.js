778cdc6e5d3b4bfeea7d5c32bce7e21a8cd46cd6
Mohammed Sahl <sahl.kably@gmail.com>
2020-04-28 00:44:42 -0400

    added all prayer timings, still have to edit CSS

--- popup.js
+++ popup.js
@@ -1,22 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-  // document.querySelector('button').addEventListener('click', onclick, false)
-  // document.querySelector('#find-me').addEventListener('click', geoFindMe);
-
     const status = document.querySelector('#status');
-    // const mapLink = document.querySelector('#map-link');
-
-    // mapLink.href = '';
-    // mapLink.textContent = '';
 
     function success(position) {
       const latitude  = position.coords.latitude;
       const longitude = position.coords.longitude;
-
+      // TODO: Add all timings
       status.textContent = '';
-      // mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      // mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-
       let httpRequest;
 
       httpRequest = new XMLHttpRequest();
@@ -25,28 +15,47 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
       }
-      httpRequest.onreadystatechange = alertContents;
-      console.log(`http://api.aladhan.com/v1/timings/${Date.now()}?latitude=${latitude}&longitude=${longitude}&method=2`);
+
+
+      httpRequest.onreadystatechange = updateContents;
+      // console.log(`http://api.aladhan.com/v1/timings/${Date.now()}?latitude=${latitude}&longitude=${longitude}&method=2`);
         httpRequest.open('GET', `http://api.aladhan.com/v1/timings/${Math.round((new Date()).getTime() / 1000)}?latitude=${latitude}&longitude=${longitude}&method=2`);
         httpRequest.send();
 
-        function alertContents() {
+        function updateContents() {
+          const prayerTimingsList = document.querySelector('#prayer-timings')
+          const lastThirdElement = prayerTimingsList.children[0];
+          const fajrElement = prayerTimingsList.children[1];
+          const dhuhrElement = prayerTimingsList.children[2];
+          const asrElement = prayerTimingsList.children[3];
+          const maghribElement = prayerTimingsList.children[4];
+          const ishaElement = prayerTimingsList.children[5];
+
           function addZero(i) {
             if (i < 10) {
               i = "0" + i;
             }
             return i;
           }
+
           if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
               today = new Date();
               res = JSON.parse(httpRequest.responseText)
-              maghrib = (res.data.timings.Maghrib).split(':')
-              fajr = (res.data.timings.Fajr).split(':')
-              let maghribDate = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate() - 1, maghrib[0], maghrib[1])
-              let fajrDate = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate(), fajr[0], fajr[1])
+              maghribTimeArray = (res.data.timings.Maghrib).split(':')
+              fajrTimeArray = (res.data.timings.Fajr).split(':')
+              let maghribDate = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate() - 1, maghribTimeArray[0], maghribTimeArray[1])
+              let fajrDate = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate(), fajrTimeArray[0], fajrTimeArray[1])
               let diff = new Date(fajrDate - ((fajrDate - maghribDate) / 3))
-              status.textContent = `The lastThird of the night starts at ${addZero(diff.getHours())}:${addZero(diff.getMinutes())}`
+              // console.log(lastThirdElement);
+              lastThirdElement.textContent += `${addZero(diff.getHours())}:${addZero(diff.getMinutes())}`;
+              fajrElement.textContent += res.data.timings.Fajr
+              dhuhrElement.textContent += res.data.timings.Dhuhr
+              asrElement.textContent += res.data.timings.Asr
+              maghribElement.textContent += res.data.timings.Maghrib
+              ishaElement.textContent += res.data.timings.Isha
+
+              // status.textContent = `The lastThird of the night starts at ${addZero(diff.getHours())}:${addZero(diff.getMinutes())}`
               // alert(`Fajr is at ${fajr} and Maghrib is at ${maghrib}`)
             } else {
               alert('There was a problem with the request.');
